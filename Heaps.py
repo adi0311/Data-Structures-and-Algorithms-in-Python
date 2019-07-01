4f4e2eb908059ab5004fe9b9240457219883d643
adityapanwar <2018308@iiitdmj.ac.in>
2019-07-01 12:38:31 +0530

    Modify Heaps.py

--- Heaps.py
+++ Heaps.py
@@ -1,3 +1,4 @@
+import sys
 class heaps:
 
     def __init__(self, n):
@@ -8,39 +9,69 @@ class heaps:
     def __len__(self):
         return len(self._data)
 
+    def parent(self, i):
+        return (i-1)//2
+
+    def leftChild(self, i):
+        return 2*i+1
+
+    def rightChild(self, i):
+        return 2*i+2
+
     def max(self):
         if self._size == 0:
             raise IndexError
-        return self._data[1]
+        return self._data[0]
 
     def insert(self, value):
         if self._size == self._maxsize:
             raise IndexError
         self._size += 1
-        i = self._size
-        while i != 1 and value > self._data[i//2]:
-            self._data[i] = self._data[i//2]
-            i = i//2
-        self._data[i] = value
-    def delete(self):
-        if self._size == 0:
+        self._data[self._size-1] = value
+        i = self._size-1
+        while i != 0 and self._data[self.parent(i)] < self._data[i]:
+            self._data[self.parent(i)], self._data[i] = self._data[i], self._data[self.parent(i)]
+            i = self.parent(i)
+
+    def Heapify(self, i):
+        l = self.leftChild(i)
+        r = self.rightChild(i)
+        largest = i
+        if l < self._size and self._data[l] > self._data[largest]:
+            largest = l
+        if r < self._size and self._data[r] > self._data[largest]:
+            largest = r
+        if i != largest:
+            self._data[i], self._data[largest] = self._data[largest], self._data[i]
+            self.Heapify(largest)
+
+    def extractMax(self):
+        if(self._size == 0):
             raise IndexError
-        max = self._data[1]
-        min = self._data[self._size]
+        if(self._size == 1):
+            self._size -= 1
+            return self._data[0]
+        root = self._data[0]
+        self._data[0] = self._data[self._size-1]
+        self._data[self._size-1] = -1
         self._size -= 1
-        i = 1
-        ci = 2
-        while ci <= self._size:
-            if ci < self._size and self._data[ci] < self._data[ci+1]:
-                ci += 1
-            if min >= self._data[ci+1]:
-                break
-            self._data[i] = self._data[ci]
-            i = ci
-            ci = ci*2
-        self._data[self._size+1] = -1
-        self._data[i] = min
-        return max
+        self.Heapify(0)
+
+    def decreaseKey(self, i, value):
+        self._data[i] = value
+        while i != 0 and self._data[self.parent(i)] < self._data[i]:
+            self._data[self.parent(i)], self._data[i] = self._data[i], self._data[self.parent(i)]
+            i = self.parent(i)
+
+    def deleteKey(self, i):
+        if i >= self._size:
+            print("Invalid Key")
+            raise IndexError
+        elif self._size == 0:
+            print("Empty Heap")
+            raise Exception
+        self.decreaseKey(i, sys.maxsize)
+        self.extractMax()
 
 
 heap = heaps(10)
@@ -50,10 +81,11 @@ heap.insert(2)
 heap.insert(20)
 heap.insert(10)
 heap.insert(12)
+heap.insert(1)
+heap.insert(30)
+heap.insert(5)
+heap.insert(8)
 print(heap._data)
-print(heap.delete())
-print(heap._data)
-print(heap.delete())
-print(heap._data)
-print(heap.delete())
+print(heap.max())
+heap.deleteKey(3)
 print(heap._data)
